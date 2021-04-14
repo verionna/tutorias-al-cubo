7c480d442bdc9e1029cd84e1c3cdfed3b5fe3a95
verionna <42081457+verionna@users.noreply.github.com>
2021-04-14 02:08:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+	var words = ['Welcome','Are you ready?'],
+	wordWrapper = document.getElementById('welcome'),
+	wordWrapperContent = wordWrapper.innerHTML,
+	addingWord = false,
+	counter = 1;
+
+setInterval(function() {
+
+if(wordWrapperContent.length > 0 && !addingWord) {
+  wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+  wordWrapperContent = wordWrapper.innerHTML;
+} else {
+	addingWord = true;
+}
+
+if(addingWord) {
+	if(wordWrapperContent.length < words[counter].length ) {
+	 wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+	 wordWrapperContent = wordWrapper.innerHTML;
+	} else {
+		if ( counter < words.length ) {
+			counter ++
+		}
+		addingWord = false;
+	}
+ }
+
+ if( counter == words.length) {
+ 	 counter = 0;
+}
+}, 120);
+
+
